50c7da9e127887012898ed8cfb1f45fb510f00bd
Moaliyou <moaliyou47@gmail.com>
2024-05-10 21:25:51 +0300

    refactor: add validation whether to start the app screen or onboarding

---
+++ app/src/main/java/com/example/mycommish/core/presentation/myapp/MyCommishAppViewModel.kt
@@ -0,0 +1,31 @@
+package com.example.mycommish.core.presentation.myapp
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mycommish.core.presentation.navigation.Route
+import com.example.mycommish.feature.onboarding.domain.use_case.AppEntryUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@HiltViewModel
+class MyCommishAppViewModel @Inject constructor(
+    appEntryUseCase: AppEntryUseCase
+): ViewModel() {
+    private val _startDestination = mutableStateOf(Route.AppStart.route)
+    val startDestination: State<String> = _startDestination
+
+    init {
+        appEntryUseCase.readAppEntry().onEach { canStartPrizeScreen ->
+            if (canStartPrizeScreen) {
+                _startDestination.value = Route.Prize.route
+            } else {
+                _startDestination.value = Route.AppStart.route
+            }
+        }.launchIn(viewModelScope)
+    }
+
+}
