a662804455a8d7b07f94d87f391c39d85509d372
Moaliyou <moaliyou47@gmail.com>
2024-05-16 21:55:15 +0300

    refactor: update route paths

--- app/src/main/java/com/example/mycommish/core/presentation/navigation/Route.kt
+++ app/src/main/java/com/example/mycommish/core/presentation/navigation/Route.kt
@@ -7,31 +7,35 @@ sealed class Route(
     val activeIcon: Int = 0,
     val inactiveIcon: Int = 0
 ) {
-    data object Dashboard : Route(
-        route = "dashboard",
-        activeIcon = R.drawable.dashboard_active_icon,
-        inactiveIcon = R.drawable.dashboard_inactive_icon
-    )
+    data object Home : Route(
+        route = "home"
+    ) {
+        data object Dashboard : Route(
+            route = "dashboard",
+            activeIcon = R.drawable.dashboard_active_icon,
+            inactiveIcon = R.drawable.dashboard_inactive_icon
+        )
 
-    data object Prize : Route(
-        route = "prize",
-        activeIcon = R.drawable.prize_active_icon,
-        inactiveIcon = R.drawable.prize_inactive_icon
-    )
+        data object Prize : Route(
+            route = "prize",
+            activeIcon = R.drawable.prize_active_icon,
+            inactiveIcon = R.drawable.prize_inactive_icon
+        )
 
-    data object TrackEarnings : Route(
-        route = "track_earnings",
-        activeIcon = R.drawable.track_earnings_active_icon,
-        inactiveIcon = R.drawable.track_earnings_inactive_icon
-    )
+        data object TrackEarnings : Route(
+            route = "track_earnings",
+            activeIcon = R.drawable.track_earnings_active_icon,
+            inactiveIcon = R.drawable.track_earnings_inactive_icon
+        )
+    }
 
-    data object AppStart : Route(
-        route = "app_start"
+    data object OnBoarding : Route(
+        route = "onboarding"
     )
 
     companion object {
-        fun getNavigationItems(): List<Route> = listOf(
-            Dashboard, Prize, TrackEarnings
+        val navigationItems = listOf(
+            Home.Dashboard, Home.Prize, Home.TrackEarnings
         )
     }
 
