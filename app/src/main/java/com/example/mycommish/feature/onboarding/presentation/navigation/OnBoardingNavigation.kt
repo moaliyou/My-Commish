7a2b3060376b318c8d0a32562da190ffa4064416
Moaliyou <moaliyou47@gmail.com>
2024-05-16 22:37:58 +0300

    refactor: rename the function navigates to home of the app

--- app/src/main/java/com/example/mycommish/feature/onboarding/presentation/navigation/OnBoardingNavigation.kt
+++ app/src/main/java/com/example/mycommish/feature/onboarding/presentation/navigation/OnBoardingNavigation.kt
@@ -1,6 +1,7 @@
 package com.example.mycommish.feature.onboarding.presentation.navigation
 
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.example.mycommish.core.presentation.navigation.Route
@@ -8,14 +9,16 @@ import com.example.mycommish.feature.onboarding.presentation.OnBoardingScreen
 import com.example.mycommish.feature.onboarding.presentation.OnBoardingUiEvent
 import com.example.mycommish.feature.onboarding.presentation.OnBoardingViewModel
 
-fun NavGraphBuilder.onBoardingScreen(onNavigateToHomeScreen: () -> Unit) {
-    composable(route = Route.AppStart.route) {
+fun NavController.navigateToHome() = navigate(Route.Home.route)
+
+fun NavGraphBuilder.onBoardingScreen(onNavigateToHome: () -> Unit) {
+    composable(route = Route.OnBoarding.route) {
         val onBoardingViewModel = hiltViewModel<OnBoardingViewModel>()
 
         OnBoardingScreen(
             onGetStarted = {
                 onBoardingViewModel.onEvent(OnBoardingUiEvent.SaveAppEntry)
-                onNavigateToHomeScreen()
+                onNavigateToHome()
             }
         )
     }
