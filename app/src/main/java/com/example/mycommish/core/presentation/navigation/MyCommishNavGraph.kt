8d1b7df9f8a79f0edf30d36c6387846808dd40c9
Moaliyou <moaliyou47@gmail.com>
2024-05-16 23:22:51 +0300

    refactor: add the navigation bar to the app

--- app/src/main/java/com/example/mycommish/core/presentation/navigation/MyCommishNavGraph.kt
+++ app/src/main/java/com/example/mycommish/core/presentation/navigation/MyCommishNavGraph.kt
@@ -5,16 +5,21 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import com.example.mycommish.R
+import com.example.mycommish.core.presentation.component.CustomNavigationBar
+import com.example.mycommish.core.presentation.navigation.Route.Companion.navigationItems
 import com.example.mycommish.feature.onboarding.presentation.navigation.navigateToHome
 import com.example.mycommish.feature.onboarding.presentation.navigation.onBoardingScreen
 
@@ -24,57 +29,72 @@ fun MyCommishNavHost(
     startDestination: String
 ) {
     val navController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination
 
-    NavHost(
-        navController = navController,
-        startDestination = startDestination,
-        modifier = modifier
-    ) {
-        onBoardingScreen(
-            onNavigateToHome = {
-                if (startDestination == Route.Home.route) {
-                    navController.navigateToHome()
-                }
+    Scaffold(
+        bottomBar = {
+            if (startDestination == Route.Home.route) {
+                CustomNavigationBar(
+                    navigationItems = navigationItems,
+                    destination = currentDestination,
+                    onSelectedNavigationItem = { route -> navController.navigate(route) }
+                )
             }
-        )
-
-        navigation(
-            route = Route.Home.route,
-            startDestination = Route.Home.Dashboard.route
+        },
+        modifier = Modifier.padding(dimensionResource(R.dimen.extra_medium_padding))
+    ) { innerPaddingValue ->
+        NavHost(
+            navController = navController,
+            startDestination = startDestination,
+            modifier = modifier.padding(innerPaddingValue)
         ) {
-            composable(route = Route.Home.Dashboard.route) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(dimensionResource(R.dimen.extra_medium_padding)),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(text = "Dashboard", style = MaterialTheme.typography.titleLarge)
+            onBoardingScreen(
+                onNavigateToHome = {
+                    if (startDestination == Route.Home.route) {
+                        navController.navigateToHome()
+                    }
                 }
-            }
+            )
 
-            composable(route = Route.Home.Prize.route) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(dimensionResource(R.dimen.extra_medium_padding)),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(text = "Prize", style = MaterialTheme.typography.titleLarge)
+            navigation(
+                route = Route.Home.route,
+                startDestination = Route.Home.Dashboard.route
+            ) {
+                composable(route = Route.Home.Dashboard.route) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(dimensionResource(R.dimen.extra_medium_padding)),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(text = "Dashboard", style = MaterialTheme.typography.titleLarge)
+                    }
                 }
-            }
 
-            composable(route = Route.Home.TrackEarnings.route) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(dimensionResource(R.dimen.extra_medium_padding)),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(text = "Track earnings", style = MaterialTheme.typography.titleLarge)
+                composable(route = Route.Home.Prize.route) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(dimensionResource(R.dimen.extra_medium_padding)),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(text = "Prize", style = MaterialTheme.typography.titleLarge)
+                    }
+                }
+
+                composable(route = Route.Home.TrackEarnings.route) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(dimensionResource(R.dimen.extra_medium_padding)),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(text = "Track earnings", style = MaterialTheme.typography.titleLarge)
+                    }
                 }
             }
         }
