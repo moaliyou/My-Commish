adcbb877d07bc835b8deb25ad13d9c4cff589608
Moaliyou <moaliyou47@gmail.com>
2024-05-17 17:54:10 +0300

    refactor: remove redundancy lambda call for navigating to home screen

--- app/src/main/java/com/example/mycommish/feature/onboarding/presentation/navigation/OnBoardingNavigation.kt
+++ app/src/main/java/com/example/mycommish/feature/onboarding/presentation/navigation/OnBoardingNavigation.kt
@@ -1,25 +1,17 @@
 package com.example.mycommish.feature.onboarding.presentation.navigation
 
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import com.example.mycommish.core.presentation.navigation.Route
 import com.example.mycommish.feature.onboarding.presentation.OnBoardingScreen
-import com.example.mycommish.feature.onboarding.presentation.OnBoardingUiEvent
 import com.example.mycommish.feature.onboarding.presentation.OnBoardingViewModel
 
-fun NavController.navigateToHome() = navigate(Route.Home.route)
 
-fun NavGraphBuilder.onBoardingScreen(onNavigateToHome: () -> Unit) {
-    composable(route = Route.OnBoarding.route) {
+fun NavGraphBuilder.onBoardingScreen() {
+    composable(route = Route.AppStart.OnBoarding.route) {
         val onBoardingViewModel = hiltViewModel<OnBoardingViewModel>()
 
-        OnBoardingScreen(
-            onGetStarted = {
-                onBoardingViewModel.onEvent(OnBoardingUiEvent.SaveAppEntry)
-                onNavigateToHome()
-            }
-        )
+        OnBoardingScreen(onGetStarted = onBoardingViewModel::onEvent)
     }
 }
