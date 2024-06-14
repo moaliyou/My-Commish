5001d6338010b3da5dc742ba0abae08364361947
moaliyou <moaliyou47@gmail.com>
2024-06-14 18:21:50 +0300

    refactor: add primary icon button

--- app/src/main/java/com/example/mycommish/feature/onboarding/presentation/component/MyCommishButton.kt
+++ app/src/main/java/com/example/mycommish/feature/onboarding/presentation/component/MyCommishButton.kt
@@ -1,40 +1,69 @@
 package com.example.mycommish.feature.onboarding.presentation.component
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mycommish.R
 import com.example.mycommish.core.presentation.ui.theme.MyCommishTheme
-import com.example.mycommish.core.presentation.ui.theme.primaryContainerBackground
-import com.example.mycommish.core.presentation.ui.theme.primaryContainerContent
 
 @Composable
-fun PrimaryContainerButton(
+fun PrimaryIconButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    contentDescription: String = "",
+    icon: ImageVector,
+) {
+    IconButton(
+        onClick = onClick,
+        colors = IconButtonDefaults.iconButtonColors(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary
+        ),
+        modifier = modifier
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = contentDescription
+        )
+    }
+}
+
+@Composable
+fun PrimaryButton(
     modifier: Modifier = Modifier,
     text: String,
     onClick: () -> Unit,
-    shape: Shape = RoundedCornerShape(dimensionResource(R.dimen.medium_padding))
+    shape: Shape = RoundedCornerShape(dimensionResource(R.dimen.large_padding))
 ) {
     Button(
         onClick = onClick,
         modifier = modifier,
         colors = ButtonDefaults.buttonColors(
-            containerColor = primaryContainerContent,
-            contentColor = primaryContainerBackground
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary
         ),
         shape = shape
     ) {
         Text(text = text)
     }
+
 }
 
 @Composable
@@ -43,7 +72,7 @@ fun SecondaryButton(
     text: String,
     contentColor: Color = Color.Unspecified,
     onClick: () -> Unit
-){
+) {
     TextButton(
         onClick = onClick,
         modifier = modifier
@@ -59,10 +88,18 @@ fun SecondaryButton(
 @Composable
 private fun ActionButtonsPreview() {
     MyCommishTheme {
-        Column {
-            PrimaryContainerButton(
-                text = "Go",
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            PrimaryIconButton(
+                contentDescription = "next",
+                icon = ImageVector.vectorResource(id = R.drawable.ic_foward_arrow),
                 onClick = {}
+            )
+            PrimaryButton(
+                text = "Go",
+                onClick = {},
             )
             SecondaryButton(
                 text = "Back",
