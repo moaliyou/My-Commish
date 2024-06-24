a5dd6d507dfbc3b4f0d958f93cfa3190ee49c23a
moaliyou <moaliyou47@gmail.com>
2024-06-24 19:14:34 +0300

    refactor: migrate to Small top app bar

--- app/src/main/java/com/example/mycommish/core/presentation/component/MyCommishTopAppBar.kt
+++ app/src/main/java/com/example/mycommish/core/presentation/component/MyCommishTopAppBar.kt
@@ -2,19 +2,23 @@
 
 package com.example.mycommish.core.presentation.component
 
-import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mycommish.R
 import com.example.mycommish.core.presentation.ui.theme.MyCommishTheme
@@ -24,11 +28,13 @@ fun MyCommishTopAppBar(
     modifier: Modifier = Modifier,
     scrollBehavior: TopAppBarScrollBehavior? = TopAppBarDefaults.pinnedScrollBehavior(),
     title: String,
-    actionIcon: Int,
-    onActionIconClick: () -> Unit,
-
+    actionIcon: Int = 0,
+    onActionClick: () -> Unit = {},
+    canNavigateBack: Boolean = false,
+    navigationUpIcon: Int = 0,
+    onNavigateUp: () -> Unit = {}
     ) {
-    CenterAlignedTopAppBar(
+    TopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.surface,
             titleContentColor = MaterialTheme.colorScheme.onSurface,
@@ -37,16 +43,29 @@ fun MyCommishTopAppBar(
             Text(
                 text = title,
                 maxLines = 1,
-                overflow = TextOverflow.Ellipsis
+                textAlign = if (canNavigateBack) TextAlign.Left else TextAlign.Center,
+                modifier = Modifier.fillMaxWidth()
             )
         },
         actions = {
-            IconButton(onClick = onActionIconClick) {
-                Icon(
-                    imageVector = ImageVector.vectorResource(actionIcon),
-                    contentDescription = title,
-                    tint = MaterialTheme.colorScheme.onSurface
-                )
+            if (!canNavigateBack) {
+                IconButton(onClick = onActionClick) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(actionIcon),
+                        contentDescription = title,
+                        tint = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+            }
+        },
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = onNavigateUp) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(navigationUpIcon),
+                        contentDescription = title
+                    )
+                }
             }
         },
         scrollBehavior = scrollBehavior,
@@ -58,10 +77,20 @@ fun MyCommishTopAppBar(
 @Composable
 private fun MyCommishTopAppBarPreview() {
     MyCommishTheme {
-        MyCommishTopAppBar(
-            title = "My Commish Top App Bar",
-            actionIcon = R.drawable.track_earnings_active_icon,
-            onActionIconClick = {}
-        )
+        Column(
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.extra_medium_padding))
+        ) {
+            MyCommishTopAppBar(
+                title = "Prize",
+                actionIcon = R.drawable.track_earnings_active_icon,
+                onActionClick = {}
+            )
+            MyCommishTopAppBar(
+                title = "Prize Entry",
+                canNavigateBack = true,
+                navigationUpIcon = R.drawable.ic_navigation_back_arrow,
+                onNavigateUp = {}
+            )
+        }
     }
 }
