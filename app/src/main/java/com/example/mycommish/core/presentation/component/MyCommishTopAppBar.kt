edaeda1fb3502bc0a11eef4d61887a7481329094
moaliyou <moaliyou47@gmail.com>
2024-06-28 18:24:23 +0300

    refactor: remove scrolling behavior

--- app/src/main/java/com/example/mycommish/core/presentation/component/MyCommishTopAppBar.kt
+++ app/src/main/java/com/example/mycommish/core/presentation/component/MyCommishTopAppBar.kt
@@ -4,6 +4,7 @@ package com.example.mycommish.core.presentation.component
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -12,7 +13,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -26,7 +26,6 @@ import com.example.mycommish.core.presentation.ui.theme.MyCommishTheme
 @Composable
 fun MyCommishTopAppBar(
     modifier: Modifier = Modifier,
-    scrollBehavior: TopAppBarScrollBehavior? = TopAppBarDefaults.pinnedScrollBehavior(),
     title: String,
     actionIcon: Int = 0,
     onActionClick: () -> Unit = {},
@@ -42,8 +41,8 @@ fun MyCommishTopAppBar(
         title = {
             Text(
                 text = title,
-                maxLines = 1,
                 textAlign = if (canNavigateBack) TextAlign.Left else TextAlign.Center,
+                maxLines = 1,
                 modifier = Modifier.fillMaxWidth()
             )
         },
@@ -68,28 +67,24 @@ fun MyCommishTopAppBar(
                 }
             }
         },
-        scrollBehavior = scrollBehavior,
         modifier = modifier
     )
 }
 
-@Preview
+@Preview(
+    showBackground = true
+)
 @Composable
 private fun MyCommishTopAppBarPreview() {
     MyCommishTheme {
         Column(
+            modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.extra_medium_padding))
         ) {
             MyCommishTopAppBar(
                 title = "Prize",
                 actionIcon = R.drawable.track_earnings_active_icon,
                 onActionClick = {}
-            )
-            MyCommishTopAppBar(
-                title = "Prize Entry",
-                canNavigateBack = true,
-                navigationUpIcon = R.drawable.ic_navigation_back_arrow,
-                onNavigateUp = {}
             )
         }
     }
