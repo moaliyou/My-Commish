934a1bfc47d51c4af7920e51f08069063e74fa57
kuredema7 <kuredema7@gmail.com>
2024-07-06 15:05:42 +0300

    refactor: rename it to PrizeObject to avoid naming conflict

--- app/src/main/java/com/example/mycommish/feature/prize/data/local/datasource/PrizeObject.kt
+++ app/src/main/java/com/example/mycommish/feature/prize/data/local/datasource/PrizeObject.kt
@@ -4,7 +4,7 @@ import io.realm.kotlin.types.RealmObject
 import io.realm.kotlin.types.annotations.PrimaryKey
 import org.mongodb.kbson.ObjectId
 
-class Prize : RealmObject {
+class PrizeObject : RealmObject {
     @PrimaryKey
     var _id: ObjectId = ObjectId()
     var name: String = ""
