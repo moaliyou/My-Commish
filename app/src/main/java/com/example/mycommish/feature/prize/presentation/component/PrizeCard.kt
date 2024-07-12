927bd02a14d70159f705dc9ad91480b3d79e37d5
Moaliyou <moaliyou47@gmail.com>
2024-07-12 15:14:11 +0300

    refactor: add menu to perform edit/remove action

--- app/src/main/java/com/example/mycommish/feature/prize/presentation/component/PrizeCard.kt
+++ app/src/main/java/com/example/mycommish/feature/prize/presentation/component/PrizeCard.kt
@@ -1,18 +1,35 @@
 package com.example.mycommish.feature.prize.presentation.component
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.mycommish.R
 import com.example.mycommish.core.presentation.ui.theme.MyCommishTheme
@@ -21,8 +38,12 @@ import com.example.mycommish.core.presentation.ui.theme.MyCommishTheme
 fun PrizeCard(
     modifier: Modifier = Modifier,
     prizeName: String,
-    prizeValue: String
+    prizeValue: String,
+    onDeleteClick: () -> Unit = {},
+    onEditClick: () -> Unit = {},
 ) {
+    var showPrizeMenu by remember { mutableStateOf(false) }
+
     Card(
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.secondaryContainer,
@@ -39,11 +60,73 @@ fun PrizeCard(
         ) {
             Text(
                 text = prizeName,
-                style = MaterialTheme.typography.titleMedium,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.weight(1f)
             )
             Text(
                 text = prizeValue,
-                style = MaterialTheme.typography.titleSmall,
+                style = MaterialTheme.typography.titleLarge.copy(fontWeight = FontWeight.SemiBold),
+            )
+            Spacer(modifier = Modifier.weight(0.1f))
+            IconButton(onClick = { showPrizeMenu = !showPrizeMenu }) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(R.drawable.ic_settings),
+                    contentDescription = stringResource(R.string.setting_icon),
+                    tint = MaterialTheme.colorScheme.onSecondaryContainer
+                )
+            }
+
+            if (showPrizeMenu) {
+                PrizeMenu(
+                    modifier = Modifier.background(MaterialTheme.colorScheme.secondaryContainer),
+                    showPrizeMenu,
+                    onDismissRequest = { showPrizeMenu = !showPrizeMenu },
+                    onDeleteClick = onDeleteClick,
+                    onEditClick = onEditClick,
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun PrizeMenu(
+    modifier: Modifier = Modifier,
+    showPrizeMenu: Boolean,
+    onDismissRequest: () -> Unit,
+    onDeleteClick: () -> Unit,
+    onEditClick: () -> Unit
+) {
+
+    Box(
+        modifier = Modifier
+            .fillMaxHeight()
+            .wrapContentSize(Alignment.BottomStart)
+    ) {
+        DropdownMenu(
+            expanded = showPrizeMenu,
+            onDismissRequest = onDismissRequest,
+            modifier = modifier
+        ) {
+            DropdownMenuItem(
+                text = { Text(stringResource(R.string.edit_menu_label)) },
+                onClick = onEditClick,
+                leadingIcon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_edit),
+                        contentDescription = stringResource(R.string.edit_menu_label)
+                    )
+                }
+            )
+            DropdownMenuItem(
+                text = { Text(stringResource(R.string.delete_menu_label)) },
+                onClick = onDeleteClick,
+                leadingIcon = {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(R.drawable.ic_delete),
+                        contentDescription = stringResource(R.string.delete_menu_label)
+                    )
+                }
             )
         }
     }
@@ -56,7 +139,7 @@ private fun PrizeCardPreview() {
         Column {
             PrizeCard(
                 prizeName = "Flex",
-                prizeValue = "$1.00"
+                prizeValue = "$1.00",
             )
         }
     }
