03cbb59fc4ffdcbec38df0fdd85db97b7faf72ee
Moaliyou <moaliyou47@gmail.com>
2024-07-13 23:43:25 +0300

    fix: validate duplicate data entry

--- app/src/main/java/com/example/mycommish/feature/prize/data/local/PrizeEntity.kt
+++ app/src/main/java/com/example/mycommish/feature/prize/data/local/PrizeEntity.kt
@@ -1,9 +1,16 @@
 package com.example.mycommish.feature.prize.data.local
 
 import androidx.room.Entity
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "prizes")
+@Entity(
+    tableName = "prizes",
+    indices = [Index(
+        value = ["name", "value"],
+        unique = true
+    )]
+)
 data class PrizeEntity(
     @PrimaryKey(autoGenerate = true)
     var id: Long = 0,
