a083d139f467b89e5ad6b67a5a0f8113e0923690
Moaliyou <moaliyou47@gmail.com>
2024-07-15 21:41:28 +0300

    fix: change parameter datatype

--- app/src/main/java/com/example/mycommish/feature/prize/domain/repository/PrizeRepo.kt
+++ app/src/main/java/com/example/mycommish/feature/prize/domain/repository/PrizeRepo.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.Flow
 
 interface PrizeRepo {
     fun getAllPrizes(): Flow<List<PrizeEntity>>
-    fun getSinglePrizeById(id: Int): Flow<PrizeEntity?>
+    fun getSinglePrizeById(id: Long): Flow<PrizeEntity?>
     suspend fun insertPrize(prizeEntity: PrizeEntity)
     suspend fun updatePrize(prizeEntity: PrizeEntity)
     suspend fun deletePrize(prizeEntity: PrizeEntity)
