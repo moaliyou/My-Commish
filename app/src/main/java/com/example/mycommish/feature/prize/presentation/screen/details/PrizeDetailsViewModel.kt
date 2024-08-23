e65bb2a71e1106ea22888c7739ad3dc20c07052e
Moaliyou <moaliyou47@gmail.com>
2024-08-23 21:22:26 +0300

    refactor: implement sorting state to update prize details on filter data change

--- app/src/main/java/com/example/mycommish/feature/prize/presentation/screen/details/PrizeDetailsViewModel.kt
+++ app/src/main/java/com/example/mycommish/feature/prize/presentation/screen/details/PrizeDetailsViewModel.kt
@@ -3,9 +3,14 @@ package com.example.mycommish.feature.prize.presentation.screen.details
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mycommish.feature.prize.domain.usecase.PrizeUseCases
+import com.example.mycommish.feature.prize.domain.util.SortTypes
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.collections.immutable.toImmutableList
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
@@ -16,18 +21,32 @@ class PrizeDetailsViewModel @Inject constructor(
     private val prizeUseCases: PrizeUseCases
 ) : ViewModel() {
 
-    var prizeDetailsUiState =
-        prizeUseCases.getPrizes().map { PrizeDetailsUiState(it.toImmutableList()) }
-            .stateIn(
-                scope = viewModelScope,
-                started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
-                initialValue = PrizeDetailsUiState()
-            )
+    private val _sortingTypeState = MutableStateFlow(SortTypes.ByName.option)
+    val sortingTypeState = _sortingTypeState.asStateFlow()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val prizeDetailsUiState = sortingTypeState.flatMapLatest { sortingTypeStateValue ->
+        prizeUseCases.getPrizes(sortingTypeStateValue)
+            .map {
+                PrizeDetailsUiState(
+                    prizeList = it.toImmutableList(),
+                    selectedSortOption = sortingTypeStateValue
+                )
+            }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
+        initialValue = PrizeDetailsUiState()
+    )
 
     fun deletePrize(id: Long) {
         viewModelScope.launch {
             prizeUseCases.deletePrize(prizeId = id)
         }
+    }
+
+    fun sortBy(sortingType: SortTypes) {
+        _sortingTypeState.value = sortingType.option
     }
 
     companion object {
