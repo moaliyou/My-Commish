995bde82c9aa31b643788c6d4746d6b53940c4f2
Moaliyou <moaliyou47@gmail.com>
2024-08-23 21:11:10 +0300

    refactor: add filter chip to the prize details screen

--- app/src/main/java/com/example/mycommish/feature/prize/presentation/screen/details/PrizeDetailsScreen.kt
+++ app/src/main/java/com/example/mycommish/feature/prize/presentation/screen/details/PrizeDetailsScreen.kt
@@ -1,46 +1,30 @@
-@file:OptIn(ExperimentalLayoutApi::class)
-
 package com.example.mycommish.feature.prize.presentation.screen.details
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
-import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Warning
-import androidx.compose.material3.FilterChip
-import androidx.compose.material3.FilterChipDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -48,22 +32,22 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.mycommish.R
 import com.example.mycommish.core.presentation.component.MyCommishAlertDialog
+import com.example.mycommish.core.presentation.component.MyCommishFilterChip
 import com.example.mycommish.core.presentation.component.MyCommishTopAppBar
 import com.example.mycommish.core.presentation.component.NoDataIndicator
 import com.example.mycommish.core.presentation.ui.theme.MyCommishTheme
 import com.example.mycommish.feature.prize.domain.model.Prize
+import com.example.mycommish.feature.prize.domain.util.SortTypes
 import com.example.mycommish.feature.prize.presentation.component.PrizeCard
 import kotlinx.collections.immutable.ImmutableList
-import kotlinx.collections.immutable.toImmutableList
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 @Composable
 fun PrizeDetailsScreen(
     onActionClick: () -> Unit,
     navigateToEditPrize: (Long) -> Unit,
     prizeDetailsUiState: PrizeDetailsUiState,
-    onDeletePrize: (Long) -> Unit
+    onDeletePrize: (Long) -> Unit,
+    onSortClick: (SortTypes) -> Unit,
 ) {
     var deletePrizeConfirmed by rememberSaveable { mutableStateOf(false) }
     var prizeId by rememberSaveable { mutableLongStateOf(0) }
@@ -90,7 +74,8 @@ fun PrizeDetailsScreen(
                 deletePrizeConfirmed = !deletePrizeConfirmed
                 prizeId = it
             },
-            onEditClick = { navigateToEditPrize(it) }
+            onEditClick = { navigateToEditPrize(it) },
+            onSortClick = onSortClick
         )
 
         if (deletePrizeConfirmed) {
@@ -116,7 +101,8 @@ private fun PrizeDetailsBody(
     prizeDetailsUiState: PrizeDetailsUiState,
     contentPadding: PaddingValues = PaddingValues(dimensionResource(R.dimen.small_padding)),
     onDeleteClick: (Long) -> Unit,
-    onEditClick: (Long) -> Unit
+    onEditClick: (Long) -> Unit,
+    onSortClick: (SortTypes) -> Unit,
 ) {
     val prizes = prizeDetailsUiState.prizeList
 
@@ -129,87 +115,24 @@ private fun PrizeDetailsBody(
             contentPadding = contentPadding
         )
     } else {
-        var selectedFilter by remember { mutableStateOf(false) }
-        var sortedPrizeList by remember { mutableStateOf(prizeDetailsUiState.prizeList) }
-        val mCoroutineScape = rememberCoroutineScope()
-
         Column(
             modifier = modifier
         ) {
-            PrizeFilterChip(
-                modifier = Modifier.padding(horizontal = dimensionResource(R.dimen.extra_medium_padding)),
-                onFilter = {
-                    selectedFilter = !selectedFilter
-                    mCoroutineScape.launch(Dispatchers.IO) {
-                        if (selectedFilter) {
-                            sortedPrizeList =
-                                sortedPrizeList.sortedByDescending { it.value }.toImmutableList()
-                        } else {
-                            sortedPrizeList =
-                                sortedPrizeList.sortedBy { it.name }.toImmutableList()
-                        }
-                    }
-                },
-                selectedFilter = selectedFilter
+            MyCommishFilterChip(
+                selectedSortOption = prizeDetailsUiState.selectedSortOption,
+                onSortClick = onSortClick,
+                sortingOptions = SortTypes.getSortOptions(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        dimensionResource(R.dimen.medium_padding)
+                    )
             )
             PrizeList(
-                prizes = sortedPrizeList,
+                prizes = prizes,
                 contentPadding = contentPadding,
                 onDeleteClick = onDeleteClick,
                 onEditClick = onEditClick
-            )
-        }
-    }
-}
-
-@Composable
-private fun PrizeFilterChip(
-    modifier: Modifier = Modifier,
-    onFilter: () -> Unit,
-    selectedFilter: Boolean = false
-) {
-    Row(
-        modifier = modifier,
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.extra_medium_padding))
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.small_padding))
-        ) {
-            Icon(
-                imageVector = Icons.Filled.Menu,
-                contentDescription = "Filter Icon",
-                modifier = Modifier.size(FilterChipDefaults.IconSize)
-            )
-            Text(
-                text = "Filter",
-                style = MaterialTheme.typography.titleMedium,
-                color = MaterialTheme.colorScheme.onSurface
-            )
-        }
-
-        FlowRow(
-            verticalArrangement = Arrangement.Center,
-            maxItemsInEachRow = 4
-        ) {
-            FilterChip(
-                onClick = onFilter,
-                label = {
-                    Text("Highest value")
-                },
-                selected = selectedFilter,
-                leadingIcon = if (selectedFilter) {
-                    {
-                        Icon(
-                            imageVector = Icons.Filled.Done,
-                            contentDescription = "Done icon",
-                            modifier = Modifier.size(FilterChipDefaults.IconSize)
-                        )
-                    }
-                } else {
-                    null
-                },
             )
         }
     }
@@ -259,7 +182,8 @@ private fun PrizeDetailsScreenPreview() {
             onActionClick = {},
             navigateToEditPrize = {},
             prizeDetailsUiState = prizeDetailsUiState,
-            onDeletePrize = {}
+            onDeletePrize = {},
+            onSortClick = {}
         )
     }
 }
